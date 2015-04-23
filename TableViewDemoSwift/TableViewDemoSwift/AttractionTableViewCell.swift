4e74b9e7eda2365f6a2eef6f5e27a2dd4475588c
kirangorad-tudip <kiran.gorad@tudip.nl>
2015-04-24 00:25:07 +0530

    Implemented UITableVIew Implemented UITableView Delegate methods

---
+++ TableViewDemoSwift/TableViewDemoSwift/AttractionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AttractionTableViewCell.swift
+//  TableViewDemoSwift
+//
+//  Created by tudip on 16/04/15.
+//  Copyright (c) 2015 MM-10. All rights reserved.
+//
+
+import UIKit
+
+class AttractionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
