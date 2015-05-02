d43937a4dcc78524422dc3a66d36a5aed083b3f3
kirangorad-tudip <kiran.gorad@tudip.nl>
2015-05-02 15:09:32 +0530

    Remoed the DBDemo project

---
+++ DBDemo/DBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DBDemo
+//
+//  Created by tudip on 02/05/15.
+//  Copyright (c) 2015 MM-10. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
